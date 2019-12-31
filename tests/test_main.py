4e15bc578b95da1d5db31dfe83dc4576745b8ff7
Axel Tillequin <bdcht3@gmail.com>
2019-12-31 17:58:08 +0100

    fix test_main force test-specific config

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,22 +3,16 @@ from click.testing import CliRunner
 from ccrawl.main import *
 
 def test_00_cmd_collect(configfile,dbfile):
-    c = conf.Config(configfile)
-    c.Terminal.quiet = True
-    c.Terminal.timer = False
-    c.Collect.strict = False
-    c.Collect.cxx    = False
-    conf.config = c
     runner = CliRunner()
-    result = runner.invoke(cli, ['-l',dbfile,'-b','None',
+    result = runner.invoke(cli, ['-l',dbfile,'-b','None', '-c', configfile,
                            'collect', os.path.join(os.path.dirname(__file__), 'samples/xxx')])
     assert result.exit_code == 0
     assert result.stdout.startswith('[100%]')
     assert 'xxx/yyy/somewhere.h' in result.stdout
 
-def test_01_cmd_search(dbfile):
+def test_01_cmd_search(configfile,dbfile):
     runner = CliRunner()
-    result = runner.invoke(cli, ['-l',dbfile,'-b','None',
+    result = runner.invoke(cli, ['-l',dbfile,'-b','None', '-c', configfile,
                            'search', '\?_\w'])
     assert result.exit_code == 0
     l = result.output.split('\n')
@@ -27,16 +21,16 @@ def test_01_cmd_search(dbfile):
     assert 'found cUnion identifer "union ?_' in result.output
 
 
-def test_02_cmd_select(dbfile):
+def test_02_cmd_select(configfile,dbfile):
     runner = CliRunner()
-    result = runner.invoke(cli, ['-l',dbfile,'-b','None',
+    result = runner.invoke(cli, ['-l',dbfile,'-b','None', '-c', configfile,
                            'select', 'constant', '10'])
     assert result.exit_code == 0
     assert result.output == 'C1\n'
 
-def test_03_cmd_select(dbfile):
+def test_03_cmd_select(configfile,dbfile):
     runner = CliRunner(mix_stderr=False)
-    result = runner.invoke(cli, ['-l',dbfile,'-b','None',
+    result = runner.invoke(cli, ['-l',dbfile,'-b','None', '-c', configfile,
             'select', 'struct', '*:1'])
     assert result.exit_code == 0
     l = result.stdout.strip().split('\n')
@@ -45,9 +39,9 @@ def test_03_cmd_select(dbfile):
     assert 'struct ?_' in l[1]
 
 
-def test_04_cmd_show(dbfile):
+def test_04_cmd_show(configfile,dbfile):
     runner = CliRunner()
-    result = runner.invoke(cli, ['-l',dbfile,'-b','None',
+    result = runner.invoke(cli, ['-l',dbfile,'-b','None', '-c', configfile,
                            'show', '-f', 'C', 'struct xt_string_info'])
     assert result.exit_code == 0
     l = result.output.split('\n')
