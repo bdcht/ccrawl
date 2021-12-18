eece424c46db32b4eb276f862d490e95f72a24e7
Axel Tillequin <bdcht3@gmail.com>
2021-12-18 20:12:04 +0100

    prepare v1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ filtered Structures/Unions, Enums, Macros and Functions prototypes.'''
 
 setup(
     name = 'ccrawl',
-    version = '1.4',
+    version = '1.5',
     description = 'C source code crawler that creates a database of C-structures, prototypes and macros',
     long_description = long_descr,
     # Metadata
@@ -25,7 +25,7 @@ setup(
     url = 'https://github.com/bdcht/ccrawl',
     setup_requires=['pytest-runner',],
     tests_require=['pytest',],
-    install_requires = ['clang',
+    install_requires = ['libclang',
                         'click',
                         'traitlets',
                         'pyparsing',
