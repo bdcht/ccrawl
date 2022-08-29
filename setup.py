cf686f32ea31096ec59674e03e10655642d02ce0
Axel Tillequin <bdcht3@gmail.com>
2022-08-29 11:30:19 +0200

    set version to 1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ filtered Structures/Unions, Enums, Macros and Functions prototypes."""
 
 setup(
     name="ccrawl",
-    version="1.7",
+    version="1.8",
     description="C source code crawler that creates a database of C-structures, prototypes and macros",
     long_description=long_descr,
     # Metadata
